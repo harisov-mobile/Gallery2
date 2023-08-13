78fdf7be1350ead0ea3379789f64e74eccdbe4a7
harisov-mobile <harisov.mobile@gmail.com>
2023-08-13 08:20:24 +0530

    Set app theme - white status bar and navigation bar

--- app/src/main/java/com/example/gallery2/MainActivity.kt
+++ app/src/main/java/com/example/gallery2/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gallery2
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gallery2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.statusBarColor = Color.GRAY
+            window.navigationBarColor = Color.GRAY
+        }
         setContentView(R.layout.activity_main)
     }
 }
